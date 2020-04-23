ce2ee34cf24cf5e1d1dbcf0dde205aba3fd8402a
Michael Cooper <macoop2363@gmail.com>
2020-04-23 14:36:05 -0600

    Move Header into its own component and add some styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import { Header } from "../Header"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Todo List</h1>
+    <div>
+      <Header />
       <input type="checkbox" id="1" name="groceries"></input>
       <label for="1">Buy groceries</label><br></br>
       <input type="checkbox" id="2" name="laundry"></input>
