d1f63129b5c0662a8b8f7bb926ce83cdd633d0ac
Michael Cooper <macoop2363@gmail.com>
2020-04-27 14:00:37 -0600

    Move mock data into src directory

--- src/data/todoData.js
+++ src/data/todoData.js
@@ -1,14 +1,18 @@
 const todoData = [
   {
+    id: 1,
     task: "do this" 
   },
   {
+    id: 2,
     task: "do that" 
   },
   {
+    id: 3,
     task: "do this or that" 
   },
   {
+    id: 4,
     task: "do nothing" 
   },
 ]
