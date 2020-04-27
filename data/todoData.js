1ac2a302902b48cf6d6425e3dbeb6a94308ecb25
Michael Cooper <macoop2363@gmail.com>
2020-04-27 13:53:06 -0600

    Add mock data to simulate an API call

---
+++ data/todoData.js
@@ -0,0 +1,16 @@
+const todoData = [
+  {
+    task: "do this" 
+  },
+  {
+    task: "do that" 
+  },
+  {
+    task: "do this or that" 
+  },
+  {
+    task: "do nothing" 
+  },
+]
+
+export default todoData
