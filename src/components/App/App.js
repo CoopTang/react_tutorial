796be9bddc0ec5da3054c896715897155650718a
Michael Cooper <macoop2363@gmail.com>
2020-04-28 15:49:41 -0600

    Refactor App from a functional component to a class-based component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,20 +7,22 @@ import { TodoItem } from "../TodoItem"
 
 import todoData from "../../data/todoData"
 
-function App() {
-  const todoItemComponents = todoData.map(item => {
-    return <TodoItem key={item.id} item={item} />
-  })
+class App extends React.Component {
 
-  return (
-    <div>
-      <Header />
-      <body className="todo-list">
-        {todoItemComponents}
-      </body>
-      <Footer />
-    </div>
-  );
+  render() {
+    const todoItemComponents = todoData.map(item => {
+      return <TodoItem key={item.id} item={item} />
+    })
+    return (
+      <div>
+        <Header />
+        <body className="todo-list">
+          {todoItemComponents}
+        </body>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
