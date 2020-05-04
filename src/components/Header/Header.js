6b00db770e06bdcaab84cfd1dd3208d61dc2ac89
Michael Cooper <macoop2363@gmail.com>
2020-05-04 14:27:53 -0600

    Add constructor to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,10 @@ import React from "react"
 import "./Header.css"
 
 class Header extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
   render() {
     return(
       <header className="navbar">Todo List</header>
