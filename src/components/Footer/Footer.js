1c14cddccc4b16c5ab39554d80fc1047d9347064
Michael Cooper <macoop2363@gmail.com>
2020-05-04 14:28:03 -0600

    Add constructor to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,10 @@ import React from "react"
 import "./Footer.css"
 
 class Footer extends React.Component {
-  
+  constructor() {
+    super()
+    this.state = {}
+  }
   render() {
     return(
       <footer className="footer">This is app is purely for learning.</footer>
