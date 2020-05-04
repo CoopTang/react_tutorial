0007de693aff6f634d070d4bccbb79dcf183fc67
Michael Cooper <macoop2363@gmail.com>
2020-05-04 17:15:43 -0600

    Add Checkbox component to TodoItem and handle clicking of the component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react"
 
 import "./TodoItem.css"
+import "../Checkbox"
+import { Checkbox } from "../Checkbox"
 
 class TodoItem extends Component {
-  constructor() {
-    super()
-    this.state = {}
+  constructor(props) {
+    super(props)
+    this.state = {
+      isCompleted: this.props.item.isCompleted,
+      task: this.props.item.task
+    }
+
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
+  }
+
+  handleCheckboxChange() {
+    this.setState(state => ({
+      isCompleted: !state.isCompleted
+    }))
   }
   
   render() {
     return(
       <div className="todo-item">
-        <input type="checkbox" checked={!this.props.item.isCompleted}></input>
-        <p>{this.props.item.task}</p>
+        <Checkbox 
+          isChecked={this.state.isCompleted}
+          onCheckboxChange={this.handleCheckboxChange}
+        />
+        <p>{this.state.task}</p>
       </div>
     )
   }
