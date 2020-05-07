25dfdd402f59dedb75263645b301177e592a98b3
Michael Cooper <macoop2363@gmail.com>
2020-05-07 15:25:18 -0600

    Remove event handler from Checkbox and replace with defaultChecked

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -2,12 +2,11 @@ import React from 'react'
 
 import "./Checkbox.css"
 
-const Checkbox = ({isChecked, onCheckboxChange}) => (
+const Checkbox = ({isChecked}) => (
   <div className="checkbox-container">
     <input className="checkbox"
       type="checkbox"
-      checked={isChecked}
-      onChange={onCheckboxChange}
+      defaultChecked={isChecked}
     />
     <span className="checkmark" />
   </div>
